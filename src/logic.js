625f990097842949711b118a5934ff1497385575
lwkchan <laurachan205@gmail.com>
2018-07-12 22:53:18 +0100

    Add Logic class

---
+++ src/logic.js
@@ -0,0 +1,7 @@
+class Logic {
+  constructor(twitterClient){
+    this.client = twitterClient
+  }
+}
+
+export default Logic;
--- test/logic.test.js
+++ test/logic.test.js
@@ -1,1 +1,10 @@
-import logic from '../src/logic'
+import Logic from '../src/logic'
+
+describe('Logic', () => {
+  it('is initialised with a Twitter property', () => {
+    const mockTwitter = {};
+    const logic = new Logic(mockTwitter);
+
+    expect(logic.client).toEqual(mockTwitter);
+  });
+});
