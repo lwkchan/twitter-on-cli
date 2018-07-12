e87ccd82be96848022b646171cf041aedeb1ffb6
lwkchan <laurachan205@gmail.com>
2018-07-12 23:16:59 +0100

    Add general file structure

--- src/logic.js
+++ src/logic.js
@@ -2,6 +2,10 @@ class Logic {
   constructor (twitterClient) {
     this.client = twitterClient
   }
+
+  postTweet (tweet) {
+
+  }
 }
 
 export default Logic;
--- test/logic.test.js
+++ test/logic.test.js
@@ -1,10 +1,27 @@
 import Logic from '../src/logic'
 
 describe('Logic', () => {
-  it('is initialised with a Twitter property', () => {
-    const mockTwitter = {};
-    const logic = new Logic(mockTwitter);
 
-    expect(logic.client).toEqual(mockTwitter);
+  let logic;
+  let mockTwitterClient;
+
+  beforeEach(() => {
+    mockTwitterClient = { post () {} };
+    logic = new Logic(mockTwitterClient);
+  });
+
+  describe('when initialised', () => {
+    it('is initialised with the right client property', () => {
+      expect(logic.client).toEqual(mockTwitterClient);
+    });
+  });
+
+  describe('postTweet', () => {
+    it('calls post on the mockTwitterClient with the necessary parameters', () => {
+      const spy = jest.spyOn(mockTwitterClient, 'post');
+      logic.postTweet('Hello World');
+
+      expect(spy).toHaveBeenCalledWith('statuses/update', {status: 'Hello World'}, callback());
+    })
   });
 });
